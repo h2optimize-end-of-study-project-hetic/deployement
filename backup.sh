fe7adb09b83061f34a191c156eea52b520b15cb1
Joachim Renevier <renevierjoachim@gmail.com>
2025-07-25 15:20:44 +0200

    update targeet

--- backup.sh
+++ backup.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d_%H-%M")
-BACKUP_DIR="/opt/postgres_backups"
+BACKUP_DIR="/etc/h2optimize/backup"
 FILENAME="postgres_backup_$DATE.sql"
 CONTAINER_NAME="postgres_prod"
-GIT_REPO_DIR="/opt/postgres_git_repo"
-BRANCH_NAME="develop"
+GIT_REPO_DIR="/etc/h2optimize/depoyement"
+BRANCH_NAME="main"
 
 mkdir -p "$BACKUP_DIR"
-
 docker exec "$CONTAINER_NAME" pg_dump -U postgres -F p > "$BACKUP_DIR/$FILENAME"
-
 cp "$BACKUP_DIR/$FILENAME" "$GIT_REPO_DIR/"
 
-cd "$GIT_REPO_DIR"
+cd "$GIT_REPO_DIR" || exit 1
 git add "$FILENAME"
 git commit -m "Backup PostgreSQL du $DATE"
 git push origin "$BRANCH_NAME"
