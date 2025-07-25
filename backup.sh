3aaa9dd9c61dab2ca46dbece4842d803483a706d
j-renevier-htc-11 <joachim.renevier@gmail.com>
2025-07-25 14:53:27 +0000

    feat (backup): new filename template

--- backup.sh
+++ backup.sh
@@ -2,14 +2,14 @@
 
 DATE=$(date +"%Y-%m-%d_%H-%M")
 BACKUP_DIR="/etc/h2optimize/deployement/backup"
-FILENAME="postgres_backup_$DATE.sql"
+FILENAME="postgres_recorded_backup_$DATE.sql"
 CONTAINER_NAME="postgres_prod"
-GIT_REPO_DIR="/etc/h2optimize/depoyement"
+GIT_REPO_DIR="/etc/h2optimize/deployement"
 BRANCH_NAME="main"
 
 mkdir -p "$BACKUP_DIR"
-docker exec "$CONTAINER_NAME" pg_dump -U postgres -F p > "$BACKUP_DIR/$FILENAME"
-cp "$BACKUP_DIR/$FILENAME" "$GIT_REPO_DIR/"
+docker exec "$CONTAINER_NAME" pg_dump -U admin -d recorded -F p > "$BACKUP_DIR/$FILENAME"
+cp "$BACKUP_DIR/$FILENAME" "$GIT_REPO_DIR/backup"
 
 cd "$GIT_REPO_DIR" || exit 1
 git add "$FILENAME"
